6d8c2ffefe540c8e9bd2d8008ca6df30a88cf30a
Chris Reiner <cjreiner@yahoo.com>
2021-05-10 17:45:48 -0400

    Initial Commit

---
+++ site.js
@@ -0,0 +1,46 @@
+//JS for Chris Reiner's personal resume site
+//April 8, 2021
+
+$(document).ready(function(){
+
+	//Email Me Index Page Click Event Handler
+	$('#emailMe').click(function(e){
+		var email = "cjreiner@yahoo.com";
+		var subject = "Hi Chris!";
+		window.location = 'mailto:' + email + '?subject=' + subject;
+	}); //end click
+
+	//404 error page back button
+	$('#customBack').click(function(e){
+		window.history.back();
+	}); //end click
+
+	//resume download
+	$('#resumeBtn').click(function(e){
+		location = "ChrisReinerResume.pdf";
+	}); //end click
+
+
+	//Analytics Events	
+	$('#emailMe').click(function(e){
+		window.dataLayer = window.dataLayer || [];
+		window.dataLayer.push({
+			'event' : 'customEvent',
+			'category' : 'click',
+			'action' : 'email',
+			'label' : 'email me'
+		});
+	}); //end click
+
+	$('#resumeBtn').click(function(e){
+		window.dataLayer = window.dataLayer || [];
+		window.dataLayer.push({
+			'event' : 'customEvent',
+			'category' : 'click',
+			'action' : 'download',
+			'label' : 'resume'
+		});
+	}); //end click
+	//End Analytics Events
+
+}); // end ready
